2702521d419dc81af2941157a92280f2a3194ee5
ctmcisco <ctmgithub@outlook.com>
2018-05-17 16:01:22 -0500

    Update chocolateyUninstall.ps1

--- Windows-Admin-Center/Tools/chocolateyUninstall.ps1
+++ Windows-Admin-Center/Tools/chocolateyUninstall.ps1
@@ -1,8 +1,8 @@
-$packageName = 'virtualmachineconverter'
+$packageName = 'WindowsAdminCenter1804.25'
 $installerType = 'MSI'
-$url = 'http://download.microsoft.com/download/9/1/E/91E9F42C-3F1F-4AD9-92B7-8DD65DA3B0C2/mvmc_setup.msi'
+$url = 'http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758 FFC7C8C8B/WindowsAdminCenter1804.25.msi'
 $validExitCodes = @(0,3010)
-$silentArgs = "{332C1E78-1D2F-4A64-B718-68095DC6254B} /qn /l*v $env:temp\uninstallvirtualmachineconverter.log"
+$silentArgs = "{332C1E78-1D2F-4A64-B718-68095DC6254B} /qn /l*v $env:temp\WindowsAdminCenter1804.25.log"
 
 Uninstall-ChocolateyPackage -PackageName $packageName `
                                 -FileType $installerType `
