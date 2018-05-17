9e954247fac177b445686d6a49a8c31876edcd54
ctmcisco <ctmgithub@outlook.com>
2018-05-17 16:18:57 -0500

    Update chocolateyUninstall.ps1

--- Windows-Admin-Center/Tools/chocolateyUninstall.ps1
+++ Windows-Admin-Center/Tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'WindowsAdminCenter1804.25'
 $installerType = 'MSI'
-$url = 'http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758 FFC7C8C8B/WindowsAdminCenter1804.25.msi'
+$url = 'http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi'
 $validExitCodes = @(0,3010)
 $silentArgs = "{332C1E78-1D2F-4A64-B718-68095DC6254B} /qn /l*v $env:temp\WindowsAdminCenter1804.25.log"
 
